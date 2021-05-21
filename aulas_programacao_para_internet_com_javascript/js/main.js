7cd8d716247b64a81576c77d9c52b560180232ec
kevinmsantos <kevinmonteirosantos@gmail.com>
2021-05-21 19:57:17 -0300

    teste de funcao

--- aulas_programacao_para_internet_com_javascript/js/main.js
+++ aulas_programacao_para_internet_com_javascript/js/main.js
@@ -16,3 +16,11 @@ console.log(frase.replace("Japão", "Brasil")); // alterando string e imprimindo
 console.log(frase.toUpperCase()); //imprimindo conteudo string no console todo em maiusculo(capitelizado)
 console.log(frase.toLowerCase()); //imprimindo conteudo string no console todo minusculo(descapitalizado)
 alert(frase.replace("Japão", "Brasil")); //alterando conteudo string e enviado alerta na pagina do navegador
+
+function removevogal (str) {
+    str.replace(/[a|e|i|o|u|á|à|ã|é|í|ó|ô|õ|ú]/gi, "");
+
+    return str;    
+}
+
+console.log(removevogal(frase))
